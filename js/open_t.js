6bb1cf5821009b8ebd3024672a53db96d99a609c
arthur21122 <arthur.cappellina6@etu.univ-lorraine.fr>
2023-10-18 15:18:34 +0200

    ivgfe

--- js/open_t.js
+++ js/open_t.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $(".content-right > div > ul > li").click(function() {
         $(".content-right .selected").removeClass("selected")
         $(this).addClass("selected")
-        $(".content-left > img").attr("src", "img/cash/" + $(this)[0].id + "-" + current_mode + ".png")
+        $(".content-left > img").attr("src", "img/tournoi/" + current_mode + "/" + $(this)[0].id + "/" + current_stack + ".png")
     })  
 
     $(".inner-navigation > ul > li").click(function() {
@@ -16,10 +16,20 @@ $(document).ready(function() {
         changeMode()
     })
 
+    $(".inner-navigation-stack > ul > li").click(function() {
+        $(".inner-navigation-stack .selected-stack").removeClass("selected-stack")
+        $(this).addClass("selected-stack")
+        current_stack = $(this)[0].id
+        loadImage()
+    })
+
     function changeMode() {
         $(".current-mode").removeClass("current-mode")
         $("." + current_mode).addClass("current-mode")
         $(".content-left > img").attr("src", "img/cash/" + $("." + current_mode + " > ul > li")[0].id + "-" + current_mode + ".png")
     }
 
+    function loadImage(){
+        $(".content-left > img").attr("src", "img/tournoi/" + current_mode + "/" + $(".selected")[0].id + "/" + current_stack + ".png")
+    }
 })
